9187405f360347850c3ac6c324b672fb8a53faa0
Gabrielle Alice Adriano <gabrielle.adriano@infraclearsalecom.onmicrosoft.com>
2023-03-28 11:06:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="container is-max-desktop px-2 py-4">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+@import "bulma\css\bulma.min.css";
+</style>
+
+<script setup>
+// imports
+import NavBar from "@/components/Layout/NavBar.vue";
+</script>
---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content mb-1">{{ note.content }}</div>
+      <div class="has-text-right has-text-grey-light">
+        <small>{{ characterLength }}</small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a @click.prevent="handleDeleteClicked" href="#" class="card-footer-item"
+        >Delete</a
+      >
+    </footer>
+  </div>
+</template>
+
+<script setup>
+// imports
+import { computed } from "vue";
+
+// props
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+});
+
+// emits
+const emit = defineEmits(["deleteClicked"]);
+
+// character length
+const characterLength = computed(() => {
+  let length = props.note.content.length;
+  let description = length > 1 ? "characters" : "character";
+  return `${length} ${description}`;
+});
+
+// handle delete clicked
+const handleDeleteClicked = () => {
+  emit("deleteClicked", props.note.id);
+};
+</script>

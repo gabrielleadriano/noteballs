f3063561abb2712e3776c32071def24d0b68f4e1
Gabrielle Alice Adriano <gabrielle.adriano@infraclearsalecom.onmicrosoft.com>
2023-07-07 16:30:53 -0300

    feat(delete-moda):criacao de modal para exclusao de notas

---
+++ src/components/Notes/ModaDeleteNote.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="modal is-active">
+		<div class="modal-background"></div>
+		<div class="modal-card">
+			<header class="modal-card-head">
+				<p class="modal-card-title">Delete note?</p>
+				<button class="delete" aria-label="close" @click="closeModal"></button>
+			</header>
+			<section class="modal-card-body">
+				Are you sure you want to delete this note?
+			</section>
+			<footer class="modal-card-foot is-justify-content-flex-end">
+				<button class="button" @click="closeModal">Cancel</button>
+				<button class="button is-danger" @click="storeNotes.deleteNote(noteId)">
+					Delete
+				</button>
+			</footer>
+		</div>
+	</div>
+</template>
+<script setup>
+// imports
+import { useStoreNotes } from "@/stores/storeNotes";
+
+// props
+const props = defineProps({
+	modelValue: {
+		type: Boolean,
+		default: false,
+	},
+	noteId: {
+		type: String,
+		required: true,
+	},
+});
+
+// emits
+const emit = defineEmits(["update:modelValue"]);
+
+// store
+const storeNotes = useStoreNotes();
+
+// close modal
+const closeModal = () => {
+	emit("update:modelValue");
+};
+</script>
--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,48 +1,59 @@
 <template>
-  <div class="card mb-4">
-    <div class="card-content">
-      <div class="content mb-1">{{ note.content }}</div>
-      <div class="has-text-right has-text-grey-light">
-        <small>{{ characterLength }}</small>
-      </div>
-    </div>
-    <footer class="card-footer">
-      <router-link
-        :to="`/editNote/${note.id}`"
-        href="#"
-        class="card-footer-item"
-        >Edit</router-link
-      >
-      <a
-        @click.prevent="storeNotes.deleteNote(note.id)"
-        href="#"
-        class="card-footer-item"
-        >Delete</a
-      >
-    </footer>
-  </div>
+	<div class="card mb-4">
+		<div class="card-content">
+			<div class="content mb-1">{{ note.content }}</div>
+			<div class="has-text-right has-text-grey-light">
+				<small>{{ characterLength }}</small>
+			</div>
+		</div>
+		<footer class="card-footer">
+			<router-link
+				:to="`/editNote/${note.id}`"
+				href="#"
+				class="card-footer-item"
+				>Edit</router-link
+			>
+			<a
+				@click.prevent="modals.deleteNote = true"
+				href="#"
+				class="card-footer-item"
+				>Delete</a
+			>
+		</footer>
+		<ModaDeleteNote
+			v-if="modals.deleteNote"
+			v-model="modals.deleteNote"
+			:noteId="note.id" />
+	</div>
 </template>
 
 <script setup>
 // imports
-import { computed } from "vue";
+import { computed, reactive } from "vue";
 import { useStoreNotes } from "@/stores/storeNotes";
+import ModaDeleteNote from "@/components/Notes/ModaDeleteNote.vue";
 
 // store
 const storeNotes = useStoreNotes();
 
 // props
 const props = defineProps({
-  note: {
-    type: Object,
-    required: true,
-  },
+	note: {
+		type: Object,
+		required: true,
+	},
 });
 
 // character length
 const characterLength = computed(() => {
-  let length = props.note.content.length;
-  let description = length > 1 ? "characters" : "character";
-  return `${length} ${description}`;
+	let length = props.note.content.length;
+	let description = length > 1 ? "characters" : "character";
+	return `${length} ${description}`;
+});
+
+// modals
+
+const modals = reactive({
+	deleteNote: false,
 });
 </script>
